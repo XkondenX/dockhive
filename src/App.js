c97618e5fea1e49d03340163656b6e72c4e79682
Dani Afandi <daniafandi70@gmail.com>
2024-03-13 21:40:48 +0000

    deploy

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          XkondenX
+        </p>
+        <a
+          className="XkondenX"
+          href="https://twitter.com/XkondenX"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          XkondenX
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
